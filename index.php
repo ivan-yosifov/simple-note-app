b397f4bb8449dd9278b1946d909e9f66e6ebd552
Ivan Yosifov <ivan_yosifov@yahoo.com>
2021-02-03 14:44:20 +0200

    Delete popup & functionality

--- index.php
+++ index.php
@@ -22,6 +22,20 @@ if(isset($_POST['addNote'])){
   $sql = "INSERT INTO notes (name) VALUES (:name)";
   $stmt = $pdo->prepare($sql);
   $stmt->execute([':name' => $note]);
+
+  header('Location: index.php');
+  exit();
+}
+
+// delete note
+if(isset($_POST['deleteNote'])){
+  $id = $_POST['deleteId'];
+  $sql = "DELETE FROM notes WHERE id = :id";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([':id' => $id]);
+
+  header('Location: index.php');
+  exit();
 }
 
 
@@ -55,12 +69,12 @@ if(isset($_POST['addNote'])){
             </thead>
             <tbody>
               <?php foreach($notes as $note): ?>
-              <tr>
+              <tr class="align-middle">
                 <th scope="row"><?php echo $count++; ?></th>
                 <td><?php echo $note->name; ?></td>
                 <td class="text-center">
-                  <a href="#" class="text-decoration-none mx-2" title="Update">✏️</a>
-                  <a href="#" class="text-decoration-none mx-2" title="Delete">❌</a>
+                  <a href="#" class="text-decoration-none mx-2" title="Update">✏️</a>                  
+                  <button type="button" class="btn btn-link text-decoration-none mx-2 deleteConfirm" data-bs-toggle="modal" data-bs-target="#deleteModal" title="Delete" data-id="<?php echo $note->id; ?>" >❌</button>
                 </td>
               </tr>
               <?php endforeach; ?>
@@ -69,6 +83,7 @@ if(isset($_POST['addNote'])){
         </div>
       </div>
     </div>
+
     <!-- Modal Add Note -->
     <div id="addModal" class="modal" tabindex="-1">
       <div class="modal-dialog">
@@ -84,8 +99,7 @@ if(isset($_POST['addNote'])){
                 <?php if(isset($_SESSION['msg'])): ?>
                 <p id="add-msg" class="text-danger"><?php echo $_SESSION['msg']; ?></p>
                 <?php endif; ?>
-              </div>
-              
+              </div>              
               <div class="d-flex justify-content-end">
                 <button type="submit" class="btn btn-success" name="addNote">Add</button>
               </div>
@@ -94,25 +108,59 @@ if(isset($_POST['addNote'])){
         </div>
       </div>
     </div>
+
+
+    <!-- Modal Delete Note -->
+    <div id="deleteModal" class="modal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header text-white bg-danger">
+            <h5 class="modal-title">Delete A Note </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>Are you sure you want to delete this note?</p>
+          </div>
+          <div class="modal-footer">
+            <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" class="delete-form d-inline-block" >
+              <input type="hidden" name="deleteId" id="deleteId" value="">
+              <button type="submit" class="btn btn-danger" name="deleteNote">Yes, Delete</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, I changed my mind</button>
+            </form>            
+          </div>
+        </div>
+      </div>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <?php if(isset($_SESSION['msg'])): ?>
     <script>new bootstrap.Modal(document.getElementById('addModal')).show();</script>
     <?php endif; ?>
     <script>
       var myModal = document.getElementById('addModal')
-      var myInput = document.getElementById('note')
 
+      // focus input field
+      var myInput = document.getElementById('note')
       myModal.addEventListener('shown.bs.modal', function () {
         myInput.focus()
       })
-    </script>
-    <script>
-      var myModal = document.getElementById('addModal')
+
+      // remove error on addModal close
       myModal.addEventListener('hidden.bs.modal', function (event) {
         var errorMessage= document.getElementById('add-msg')
         if(errorMessage) errorMessage.remove()
       })
 
+
+      // add id to deleteModal
+      var deleteBtns = document.getElementsByClassName('deleteConfirm');
+      for (let i = 0; i < deleteBtns.length; i++){
+        deleteBtns[i].addEventListener('click', function(e){
+          var id = e.target.dataset.id;
+          document.getElementById('deleteId').setAttribute('value', id);   
+        })
+      }
+          
     </script>
   </body>
 </html>
